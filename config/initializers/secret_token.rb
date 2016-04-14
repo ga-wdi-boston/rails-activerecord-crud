1aaec0382f47065ebcd5d9ff5fa5dc59e1c6f1eb
Ross <rdegnen@gmail.com>
2016-04-14 13:46:23 -0400

    ignored secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-RailsCrud::Application.config.secret_token = '17755d14d42775b28ad5551e711e9018e18da3c0123cc14626bce0fb545176ec8bc22c6b5d9250bead1ab276509a7e2b4f1e5a958dd2e48abeffaf4197018212'
+# RailsCrud::Application.config.secret_token = '17755d14d42775b28ad5551e711e9018e18da3c0123cc14626bce0fb545176ec8bc22c6b5d9250bead1ab276509a7e2b4f1e5a958dd2e48abeffaf4197018212'
