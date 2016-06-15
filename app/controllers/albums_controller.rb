7e218929c69d99c17f9a3b325aa0ed432570bde1
berziiii <brian@offpistedesigns.com>
2016-06-15 19:55:07 -0400

    Add started code for Albums and Songs in controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,13 @@
+# Albums Controller
 class AlbumsController < ApplicationController
+
+  def index
+    render json: albums
+  end
+
+  def show
+    id = params[:id].to_i
+    render json: albums.find { |album| album[:id] == id }
+  end
+
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,11 @@
+# Songs Controller
 class SongsController < ApplicationController
+  def index
+    render json: songs
+  end
+
+  def show
+    id = params[:id].to_i
+    render json: songs.find { |song| song[:id] == id }
+  end
 end
