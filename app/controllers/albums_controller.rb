b047431ded50d8c12090061828081e798dca0214
berziiii <brian@offpistedesigns.com>
2016-06-16 09:03:08 -0400

    Add controller and model code for index, and create

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,13 +2,29 @@
 class AlbumsController < ApplicationController
 
   def index
-    album = Album.all
-    render json: album
+    albums = Album.all
+    render text: albums
   end
 
   def show
     id = params[:id].to_i
-    render json: Album.find { |album| album[:id] == id }
+    render json: Album.find(id)
+  end
+
+  # def new
+  #   album = Album.new
+  # end
+
+  def create
+    @album = Album.new(name: params[:name], artist: params[:artist])
+    # if @album.save
+    #   render :action => 'index'
+    # end
+    # album = Album.new(params[:name, :artist, :tracks])
+    # if album.save
+    #   flash('You have successfully added an Album!')
+    # else
+    #   flash('Error')
   end
 
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,25 @@
 class Album # < ActiveRecord::Base
+  attr_accessor :name, :artist
+  @@collection = []
+
+  def initialize(id:, name:, artist:)
+    @id = id
+    @name = name
+    @artist = artist
+    @tracks = []
+
+    @@collection << self
+  end
+
   def self.all
-    album.each { |e| e }
+    @@collection
+  end
+
+  def self.find(id)
+    @@collection.select { |album| album[:id] == id} #{ |album| album.id == params[:id] }
   end
 end
+
+# we dont actually do this here...it's for sample data
+Album.new(id: 1, name: '1989', artist: 'Taytay')
+Album.new(id: 2, name: '21', artist: 'Adele')
